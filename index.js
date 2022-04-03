4ef6458d26ef7e5017eefb5dd2154365bdbdcea3
Anastasiya-Shir <86313786+Anastasiya-Shir@users.noreply.github.com>
2022-04-03 18:58:54 +0300

    switch

--- index.js
+++ index.js
@@ -169,3 +169,29 @@
 //   document.writeln(`количество интераций ${i + '<br>'}`);
 // }
 // countInteration() 
+
+
+
+//switc;
+
+let num = prompt(" Введите число от 1 до 4")
+let result;
+num = Number(num);
+switch (num) {
+  case 1:
+    result = "зима";
+    break;
+  case 2:
+    result = "весна";
+    break;
+  case 3:
+    result = "лето";
+    break;
+  case 4:
+    result = "осень";
+    break;
+  default:
+    result = "Вы ввели не то число";
+}
+document.writeln(result);
+
